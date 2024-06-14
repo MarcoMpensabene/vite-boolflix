d25485e25d3863edca9703a6e882de62287612b2
MarcoMpensabene <marcompensabene@gmail.com>
2024-06-14 12:03:19 +0200

    tested api with a static query to be sure key and infrastructure works (added axios)

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,9 +3,17 @@
 </script>
 
 <template>
-    <div>CIAOOOOOOOOOO</div>
+    <div>
+        <label for="title">Type films title</label>
+        <input type="text" id="title-search" name="title-search">
+        <button>PRESS</button>
+    </div>
+
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+    input{
+        margin : 0 1rem 0 ;
+        width: 200px;
+    }
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,36 @@
-<script >
-
+<script>
+import axios from "axios";
+export default {
+    data() {
+        return {
+            filmsList : []
+            }
+        },
+        methods : {
+        getFilms(){
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=391d0ce22ac893e9dbe22be6b0ef6c6a&query=ciao')
+                .then( (response) => {
+                    console.log(response.data.results);
+                    this.filmsList = response.data.results;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .finally(function () {
+                });
+        },
+    },
+    created(){
+        this.getFilms();
+    }
+}
 </script>
 
 <template>
-    <div>
-        Caiooooooooo
+    <div >
+        <ul>
+            <li v-for="info in filmsList" :key="info.id">{{ info.title}}</li>
+        </ul>
     </div>
 </template>
 
