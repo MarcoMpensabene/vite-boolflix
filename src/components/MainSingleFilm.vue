060f0049367ca47e6efeda4b8e8d50262a9da081
MarcoMpensabene <marcompensabene@gmail.com>
2024-06-17 11:49:17 +0200

    added image path , change vote in integer value from 1 to 5 , added descrittive strings in the list

--- src/components/MainSingleFilm.vue
+++ src/components/MainSingleFilm.vue
@@ -17,10 +17,10 @@ export default {
     <ul>
         <img v-if="filmsList.poster_path" :src="`https://image.tmdb.org/t/p/w342/${filmsList.poster_path}`" alt="">
         <img v-else src="https://ih0.redbubble.net/image.5218811881.3250/raf,360x360,075,t,fafafa:ca443f4786.jpg" alt="">
-        <li>{{ filmsList.title }} </li>
-        <li>{{ filmsList.original_title }} </li>
-        <li><span class="lang-icon" :class="`lang-icon-${filmsList.original_language}`"></span></li>
-        <li>{{ filmsList.vote_average }}</li>
+        <li> Title : {{ filmsList.title }} </li>
+        <li>Original Title :{{ filmsList.original_title }} </li>
+        <li>Original Langueage : <span class="lang-icon" :class="`lang-icon-${filmsList.original_language}`"></span></li>
+        <li >Average Vote : {{parseInt(filmsList.vote_average/2)}}</li>
     </ul>
 </template>
 
--- src/components/MainSingleSeries.vue
+++ src/components/MainSingleSeries.vue
@@ -17,10 +17,10 @@ export default {
     <ul>
         <img v-if="listSeries.poster_path" :src="`https://image.tmdb.org/t/p/w342/${listSeries.poster_path}`" alt="">
         <img v-else src="https://ih0.redbubble.net/image.5218811881.3250/raf,360x360,075,t,fafafa:ca443f4786.jpg" alt="">
-        <li>{{ listSeries.original_name }} </li>
-        <li>{{ listSeries.name }} </li>
-        <li><span class="lang-icon" :class="`lang-icon-${listSeries.original_language}`"></span></li>
-        <li>{{ listSeries.vote_average }}</li>
+        <li>Title : {{ listSeries.original_name }} </li>
+        <li>Original Title :{{ listSeries.name }} </li>
+        <li>Original Langueage : <span class="lang-icon" :class="`lang-icon-${listSeries.original_language}`"></span></li>
+        <li>Average Vote : {{parseInt(listSeries.vote_average/2)}}</li>
     </ul>
 </template>
 
